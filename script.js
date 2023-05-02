217611932c7bc101d0b7a5a82f7f0a4901d48b34
Kassio Kennedy <95866074+KaessSantos@users.noreply.github.com>
2023-05-02 18:58:47 -0300

    menu

--- script.js
+++ script.js
@@ -1,10 +1,17 @@
-"user-strict"
+"user-strict";
 
 
 
-function menu(){
-    let ul = document.querySelector('ul')
-    ul.classList.toggle("remove")
+function menu() {
+  let ul = document.querySelector("ul");
+  let listaLink = document.querySelectorAll("nav ul li a");
+  ul.classList.toggle("remove");
 
-    ul.style.transition = 'ease 1s'
+  ul.style.transition = "ease 1s";
+
+  if(listaLink !== -1){
+  
+  }
+  
 }
+
