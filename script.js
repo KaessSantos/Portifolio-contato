2fba54d0cf27c6e8c84e73694de3ac73fe1436e7
Kassio Kennedy <95866074+KaessSantos@users.noreply.github.com>
2023-05-02 21:58:25 -0300

    commit

--- script.js
+++ script.js
@@ -1,18 +1,19 @@
 "user-strict";
 
-let linkMenu = document.querySelector('ul')
+let linkMenu = document.querySelector("ul");
+
+// let creatImg = document.createElement("li");
+// creatImg.innerHTML = `<img src="imagens/kaess.png" alt="" style="width:50px">`;
+// linkMenu.append(creatImg)
+
 
 function menu() {
   let ul = document.querySelector("ul");
   let listaLink = document.querySelectorAll("nav ul li a");
   ul.classList.toggle("remove");
-
   ul.style.transition = "ease 1s";
 }
 
-linkMenu.addEventListener('click', function(){
+linkMenu.addEventListener("click", function () {
   linkMenu.classList.toggle("remove");
 });
-
-
-
