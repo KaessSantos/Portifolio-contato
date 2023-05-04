adfd8b38e9f1f2e21b788eb8aeda85c9a6d60289
Kassio Kennedy <95866074+KaessSantos@users.noreply.github.com>
2023-05-03 22:07:47 -0300

    commit

--- script.js
+++ script.js
@@ -8,17 +8,18 @@ let menuCloseX = document.querySelector('#ntn-close-menu')
 function menu() {
   linkMenu.classList.toggle("remove");
   linkMenu.style.transition = "ease .5s";
+  
   if(linkMenu.classList.contains('remove')){
     menuHamburguer.innerHTML = menuCloseX.innerHTML
   }else{
    
     menuHamburguer.innerHTML = `<i class="fa-solid fa-bars"></i>`
-
+  
   }
 }
 
-
-
-linkMenu.addEventListener("click", function(el) {
-  linkMenu.classList.toggle("remove");
+linkMenu.addEventListener("click", function(){
+ menu()
 });
+
+
